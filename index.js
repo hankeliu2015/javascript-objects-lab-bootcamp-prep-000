389ee262afc23c25d421bd617607c3755ae3cb76
Hank Liu <hanke.liu@gmail.com>
2018-08-02 03:30:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var recipes = new Object ({water: '1'});
 
+<<<<<<< HEAD
 // update 
 
 function updateObjectWithKeyAndValue(object, key, value) {
@@ -9,21 +10,45 @@ function updateObjectWithKeyAndValue(object, key, value) {
 
 function updateObjectWithKeyAndValue(object, key, value) {
   object[key] = value;
+=======
+// function updateObjectWithKeyAndValue(object, key, value) {
+//   return object.assign({}, {key: value});
+
+// }
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  object.key = value;
+>>>>>>> 48d80b464df04ea8b5ab7627ed9232324ebb4ced
   return object;
 }
 
 
+<<<<<<< HEAD
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value; // this is working by change to [].
+=======
+
+//destructive Update 
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object.key = value;
+>>>>>>> 48d80b464df04ea8b5ab7627ed9232324ebb4ced
   return object;
 }
 
 // non-destructive delete
 
+<<<<<<< HEAD
 // function deleteFromObjectByKey(object, key) {
 //   delete object.key;
 //   return object;
 // }
+=======
+function deleteFromObjectByKey(object, key) {
+  delete object.key;
+  return object;
+}
+>>>>>>> 48d80b464df04ea8b5ab7627ed9232324ebb4ced
 
 function deleteFromObjectByKey(object, key) {
   var newObj = Object.assign({}, {object:key}); // Object need to be upper case
@@ -34,7 +59,11 @@ function deleteFromObjectByKey(object, key) {
 // destructive delete
 
 function destructivelyDeleteFromObjectByKey(object, key) {
+<<<<<<< HEAD
   delete object[key];   //this worked by change from . notatiaon to []. 
+=======
+  Object.assign({}, {object, key});
+>>>>>>> 48d80b464df04ea8b5ab7627ed9232324ebb4ced
   return object;
   
 }
